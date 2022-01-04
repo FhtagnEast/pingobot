47e5b14dcbc365a1fcb4381097cda19bf28c13b8
fhtagneast <vchernogorov@yandex.ru>
2022-01-04 08:39:01 +0300

    #1 Migration sql improvement

--- src/migration.sql
+++ src/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS notifications
     user_id BIGINT,
     chat_id BIGINT,
     update_collector_message_id BIGINT,
-    edit_message_id BIGINT,
+--     edit_message_id BIGINT,
     description VARCHAR(255),
     active BOOLEAN,
     deleted BOOLEAN,
@@ -14,4 +14,11 @@ CREATE TABLE IF NOT EXISTS notifications
     last_execution BIGINT,
     created BIGINT,
     marked_on_deletion BIGINT
+);
+
+CREATE TABLE IF NOT EXISTS edit_message_ids
+(
+    notification_id BIGINT,
+    edit_message_id BIGINT,
+    FOREIGN KEY (notification_id) REFERENCES notifications (id)
 );
