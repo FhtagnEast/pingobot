4de6cb72874a975542b243efef78ace10e24816c
fhtagneast <vchernogorov@yandex.ru>
2022-01-04 08:27:13 +0300

    #1 LastCompletedTime added

--- src/main/java/com/withlava/pingobot/database/model/ExecutionInfo.java
+++ src/main/java/com/withlava/pingobot/database/model/ExecutionInfo.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 public class ExecutionInfo {
     private final long nextExecutionTime;
     private final Optional<Long> lastExecutionTime;
+    private final Optional<Long> lastCompletedTime;
 
-    public ExecutionInfo(long nextExecutionTime, Optional<Long> lastExecutionTime) {
+    public ExecutionInfo(long nextExecutionTime, Optional<Long> lastExecutionTime, Optional<Long> lastCompletedTime) {
         this.nextExecutionTime = nextExecutionTime;
         this.lastExecutionTime = lastExecutionTime;
+        this.lastCompletedTime = lastCompletedTime;
     }
 
     public long getNextExecutionTime() {
@@ -17,5 +19,9 @@ public class ExecutionInfo {
 
     public Optional<Long> getLastExecutionTime() {
         return lastExecutionTime;
+    }
+
+    public Optional<Long> getLastCompletedTime() {
+        return lastCompletedTime;
     }
 }
