166b960eb19b6f5ef2fa6dda029f6c1b5c4b56ad
fhtagneast <vchernogorov@yandex.ru>
2022-01-20 23:48:54 +0300

    * Removed unused classes * Context closing fixed * Notifier worker disabled

--- src/main/java/com/withlava/pingobot/Application.java
+++ src/main/java/com/withlava/pingobot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
-
+import org.telegram.telegrambots.meta.generics.BotSession;
 
 public class Application {
     private Application() {
@@ -17,15 +17,14 @@ public class Application {
 
     public static void main(String... args) {
         ApiContextInitializer.init();
-        Pingobot bot;
-        try (GenericXmlApplicationContext context = new GenericXmlApplicationContext()) {
-            context.load("classpath:config/bot-context.xml");
-            context.refresh();
-            bot = context.getBean(Pingobot.class);
-        }
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext();
+        context.load("classpath:config/bot-context.xml");
+        context.refresh();
+        Pingobot bot = context.getBean(Pingobot.class);
+        TelegramBotsApi api = new TelegramBotsApi();
         try {
-            final TelegramBotsApi api = new TelegramBotsApi();
-            api.registerBot(bot);
+            BotSession botSession = api.registerBot(bot);
+
         } catch (TelegramApiRequestException e) {
             logger.warn("Exception catched while try to register bot.", e);
         }
--- src/main/java/com/withlava/pingobot/notifier/NotifierWorker.java
+++ src/main/java/com/withlava/pingobot/notifier/NotifierWorker.java
@@ -1,10 +1,14 @@
 package com.withlava.pingobot.notifier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class NotifierWorker {
+    private static final Logger logger = LoggerFactory.getLogger(NotifierWorker.class);
 
     private final ScheduledExecutorService executor;
 
@@ -16,10 +20,12 @@ public class NotifierWorker {
     }
 
     public void start() {
+        logger.info("Notifier worker started!");
         executor.scheduleWithFixedDelay(notifierTask, 1, 1, TimeUnit.SECONDS);
     }
 
     public void stop() {
+        logger.info("Notifier worker stopped!");
         executor.shutdown();
     }
 }
